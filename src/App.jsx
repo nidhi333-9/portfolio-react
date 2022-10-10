0a13b55ffb031536a0842275b28a66374e39dc9f
nidhikumari <kumari9936nidhi@gmail.com>
2022-10-10 18:11:09 +0530

    fixed the top margin

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
       <main className='bg-white pb-10 px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
         <section className='min-h-screen'>
-          <nav className='py-6 mt-7 sm:mb-12 flex justify-between'>
+          <nav className='py-6 pt-16 sm:mb-12 flex justify-between'>
 
             <h1 className='text-xl font-burtons uppercase dark:text-white'>TechyNidhi</h1>
             <ul className='flex items-center'>
